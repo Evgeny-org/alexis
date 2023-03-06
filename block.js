08038a7e6a865db7d467944e44d33a0667976f8d
Евгений <evgenij@mpro-evgeny.res.rea.ru>
2023-03-06 20:48:57 +0300

    the end

---
+++ block.js
@@ -0,0 +1,29 @@
+const block1 = document.querySelector('main .ourTeam .container .blocks .block:nth-child(1)')
+const comm1 = document.querySelector('main .ourTeam .container .comment#first')
+const comm1S = window.getComputedStyle(comm1)
+
+const block2 = document.querySelector('main .ourTeam .container .blocks .block:nth-child(2)')
+const comm2 = document.querySelector('main .ourTeam .container .comment#second')
+const comm2S = window.getComputedStyle(comm2)
+
+const block3 = document.querySelector('main .ourTeam .container .blocks .block:nth-child(3)')
+const comm3 = document.querySelector('main .ourTeam .container .comment#third')
+const comm3S = window.getComputedStyle(comm3)
+
+const block4 = document.querySelector('main .ourTeam .container .blocks .block:nth-child(4)')
+const comm4 = document.querySelector('main .ourTeam .container .comment#fourh')
+const comm4S = window.getComputedStyle(comm4)
+
+// const block = document.querySelectorAll('main .development .block') //обращение ко всему элементу
+// for (let blocks of block){
+// }
+
+block2.addEventListener("click", function(e){
+	if () {}
+
+
+	// comm2.style.display = "flex"
+	// comm1.style.display = "none"
+	// comm3.style.display = "none"
+	// comm4.style.display = "none"
+})
